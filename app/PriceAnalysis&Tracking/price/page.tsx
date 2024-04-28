3604ce7ed4d914457351d1f0fae93bdf13c96662
isaac opini <isaacopini8@gmail.com>
2024-04-28 17:20:46 +0300

    resolved conficts

--- app/PriceAnalysis&Tracking/price/page.tsx
+++ app/PriceAnalysis&Tracking/price/page.tsx
@@ -54,9 +54,9 @@ export default function ProductTable() {
   };
   return (
     <div className="p-4 bg-white rounded shadow">
-      <div className="flex justify-center items-center mb-4">
-     
-        
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-xl min-w-36 font-bold">Product Table</h2>
+        <div className="flex items-center">
           <input
             className="border border-gray-300 rounded p-2 w-96"
             type="text"
@@ -64,7 +64,6 @@ export default function ProductTable() {
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
-         
         </div>
       </div>
       <div className="overflow-auto max-h-96">
