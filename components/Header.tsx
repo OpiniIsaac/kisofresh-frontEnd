25c775f9b3f1861bc38a76bec9a39dd51dcd1ec3
GRACENOBLE <gracenoble72@gmail.com>
2024-04-30 03:37:44 +0300

    fixing underline overlap

--- components/Header.tsx
+++ components/Header.tsx
@@ -72,7 +72,7 @@ export default function Header() {
                           : "relative hover:text-black transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%] hidden md:block"
                       }`,
                       {
-                        "underline underline-offset-[4px] u ":
+                        "underline underline-offset-[5px] u ":
                           pathname === item.route,
                       }
                     )}
