6da045815211e4f21bfab5e6cc0431e1e430b698
isaac opini <isaacopini8@gmail.com>
2024-05-09 11:39:49 +0300

    fixed typo

--- app/SourceProduce/page.tsx
+++ app/SourceProduce/page.tsx
@@ -454,7 +454,7 @@ export default function CropInterestForm() {
                           </td>
                           <td className="px-6 py-4 whitespace-nowrap">
                             <Button>
-                              <Link href="/SourceProduce/Form">Request Quot</Link>
+                              <Link href="/SourceProduce/Form">Request Quote</Link>
                             </Button>
                           </td>
                         </tr>
