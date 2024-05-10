1e5a0f17706ecd216e1928b866ac6737069b89a7
isaac opini <isaacopini8@gmail.com>
2024-05-10 18:47:50 +0300

    done with weather

--- app/PriceAnalysis&Tracking/weather/page.tsx
+++ app/PriceAnalysis&Tracking/weather/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default async function page() {
 
   const res = await fetchWeatherForecast('kampala')
-  console.log(res)
+  console.log(res.list)
   return (
     <div className="h-screen flex justify-center items-center">
       <h1>Weather</h1>
