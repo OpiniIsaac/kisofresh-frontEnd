4cd294293b26acc43400284cb32b71945b98fcb5
GRACENOBLE <gracenoble72@gmail.com>
2024-05-23 07:37:31 +0300

    added a navigation loading state

---
+++ app/loading.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center'>
+      <Image
+        src="/images/Logo.png"
+        alt=""
+        width={1000}
+        height={1000}
+        className="animate-bounce w-40 md:pb-40w-40 md:pb-40"
+      />
+    </div>
+  );
+}
