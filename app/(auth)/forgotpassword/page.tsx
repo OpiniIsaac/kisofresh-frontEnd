3a48611c03ac0da723c4aa5d34e1221c864280a5
isaacopini <isaacopini8@gmail.com>
2024-07-29 20:16:52 +0300

    fnished password

--- app/(auth)/forgotpassword/page.tsx
+++ app/(auth)/forgotpassword/page.tsx
@@ -1,58 +1,69 @@
 "use client";
 
-import { Login } from "@/lib/features/accountHandle/loginSlice";
-import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
-export default function page() {
-  const dispatch = useDispatch();
+import swal from "sweetalert";
+import { auth } from "@/app/firebase/config";
+import Link from "next/link";
+
+
+export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+
+
+  const handleForgotPassword = async () => {
+    try {
+      await sendPasswordResetEmail(auth, email);
+      swal({
+        title: "Email Sent",
+        text: "Please check your email for password reset instructions.",
+        icon: "success",
+      });
+      
+    } catch (error) {
+      swal({
+        title: "Error",
+        text: "Error Occcured",
+        icon: "error",
+      });
+    }
+  };
+
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-full max-w-xs ">
+      <div className="w-full max-w-xs">
         <form className="bg-blue-500/10 hover:shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="new password"
+              htmlFor="email"
             >
-              New password
+              Email
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="new password"
-              type="password"
-              placeholder="********"
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="confirm new password"
-            >
-             Confirm new password
-            </label>
-            <input
-              className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="confirm new password"
-              type="password"
-              placeholder="********"
+              id="email"
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="flex justify-end w-full">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
-              onClick={() => dispatch(Login())}
+              onClick={handleForgotPassword}
             >
-              Confirm
+              Reset Password
             </button>
-            {/* <Link
-              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              href="/forgotpassword"
-            >
-              Forgot Password?
-            </Link> */}
+          </div>
+          <div className="flex justify-end w-full mt-4">
+            <Link href="/login" className="text-blue-500 hover:text-blue-700 text-sm">
+              Back to login
+            </Link>
           </div>
         </form>
       </div>
