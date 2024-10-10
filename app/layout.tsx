efc1bd7dc0680f3008eed9bdc26b44a7a8a7c456
isaacopini <isaacopini8@gmail.com>
2024-10-10 12:38:18 +0300

    added analystics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 // const metadata = {
 //   title: "KisoIndex",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <Header />
           <main className="min-h-screen">{children}</main>
           <Footer />
+          <Analytics />
         </body>
       </html>
     </StoreProvider>
