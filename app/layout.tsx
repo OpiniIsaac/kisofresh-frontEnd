ece3282c20ffcf4bac0b317c69d1aa94bfdc7812
isaacopini <isaacopini8@gmail.com>
2024-10-10 12:40:31 +0300

    added analystics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // const metadata = {
 //   title: "KisoIndex",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <main className="min-h-screen">{children}</main>
           <Footer />
           <Analytics />
+          <SpeedInsights />
         </body>
       </html>
     </StoreProvider>
